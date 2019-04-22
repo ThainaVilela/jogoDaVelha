ddef5403d17ae8c943836ac283c43bb1d2ba0033
unknown <thainaferreiravilela@gmail.com>
2019-04-22 11:55:00 -0300

    tentativa de javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+O primeiro click resultará no aparecimento da imagem do x dentro do quadrado;
+O segundo click aparecerá a bolinha e assim irão se intercalando;
+O 
+
+
+
+
+*/
+/* declarar de quem é a vez e quem é o vencedor quando ele for definido*/
+let vez = 1;
+let vencedor = "";
+
+
+(function(){
+    //O conteúdo deve ficar aqui
+
+    });
